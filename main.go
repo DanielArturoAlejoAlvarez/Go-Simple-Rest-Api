cbc1928e276637839956028a5366ace7a8137aa3
Daniel Arturo Alejo Alvarez <yourmediasoft@gmail.com>
2021-04-13 10:09:53 -0500

    Add createTask method and config routes

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+//Model
 type task struct {
 	ID      int    `json:'ID'`
 	Name    string `json:'Name'`
@@ -26,7 +28,33 @@ var tasks = allTasks{
 }
 
 func getTasks(w http.ResponseWriter, r *http.Request) {
+	//Headers
+	w.Header().Set("Content-Type", "application/json")
+
 	json.NewEncoder(w).Encode(tasks)
+}
+
+func createTask(w http.ResponseWriter, r *http.Request) {
+	var newTask task
+
+	//Add ioutil module
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Insert a task valid")
+	}
+
+	json.Unmarshal(reqBody, &newTask)
+
+	//Generate ID thought index
+	newTask.ID = len(tasks) + 1
+	tasks = append(tasks, newTask)
+
+	//Headers
+	w.Header().Set("Content-Type", "application/json")
+	//Sending Status
+	w.WriteHeader(http.StatusCreated)
+
+	json.NewEncoder(w).Encode(newTask)
 }
 
 func indexRoute(w http.ResponseWriter, r *http.Request) {
@@ -38,7 +66,8 @@ func main() {
 	router := mux.NewRouter().StrictSlash(true)
 
 	router.HandleFunc("/", indexRoute)
-	router.HandleFunc("/api/tasks", getTasks)
+	router.HandleFunc("/api/tasks", getTasks).Methods("GET")
+	router.HandleFunc("/api/tasks", createTask).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(":9000", router))
 }
