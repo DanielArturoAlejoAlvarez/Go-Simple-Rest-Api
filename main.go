bc1bb1ae02e1f863f4c74fec9014dc07d72dd654
Daniel Arturo Alejo Alvarez <yourmediasoft@gmail.com>
2021-04-13 11:11:19 -0500

    Create getTask and route

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -32,6 +33,26 @@ func getTasks(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	json.NewEncoder(w).Encode(tasks)
+}
+
+func getTask(w http.ResponseWriter, r *http.Request) {
+	//Get ID
+	params := mux.Vars(r)
+	taskID, err := strconv.Atoi(params["id"])
+
+	if err != nil {
+		fmt.Fprintf(w, "ID invalid!")
+		return
+	}
+
+	//Get obj task from tasks array
+	for _, t := range tasks {
+		if t.ID == taskID {
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(t)
+		}
+	}
+
 }
 
 func createTask(w http.ResponseWriter, r *http.Request) {
@@ -67,6 +88,7 @@ func main() {
 
 	router.HandleFunc("/", indexRoute)
 	router.HandleFunc("/api/tasks", getTasks).Methods("GET")
+	router.HandleFunc("/api/tasks/{id}", getTask).Methods("GET")
 	router.HandleFunc("/api/tasks", createTask).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(":9000", router))
